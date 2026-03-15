c4e5c82836e1374efcfea0189410c5b4c25f38d5
AbelPhilippe <abelphilipe@live.com>
2026-03-15 04:46:09 -0300

    update seeker

--- seeker.py
+++ seeker.py
@@ -7,10 +7,11 @@ import scipy.integrate as integrate
 
 sigma = 0               # line-of-sight vector from seeker to tracking point (m)
 
-d1 = 0                  # distance 
+d1 = 0                  # distance vector from missile center of mass to seeker (m)
 
-d2 = 0
+d2 = 0                  # distance vector from target center of mass to tracking point
 
-R = 0
+R = 0                   # range vector from missile center of mass to target center of mass, m
 
-def seeker()
+def seeker():
+    sigma = 
